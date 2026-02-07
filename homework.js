60a9375c43f1f216d3addbe6f80c664f446d7ea2
khaivinh <duongkhaivinh.2008@gamil.com>
2026-02-07 23:47:48 +0700

    new file

---
+++ homework.js
@@ -0,0 +1,91 @@
+let a = 10
+b = 1
+c = 1  
+if (a >= 0 ) {
+    console.log("a là số nguyên dương")}
+        for ( b % 2 == 0; b < a; b++) {
+            console.log("tổng số nguyên dương chẳn:",b)}
+        for ( c % 2 != 0; c < a; c++) {
+            console.log("tổng số nguyên dương lẻ:",c)    
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
